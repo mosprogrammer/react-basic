70b03624ea77c98d8d0b8f5c8e067cea859b2896
mosukung <satawat.rattha@gmail.com>
2021-04-25 09:48:26 +0700

    list-and-key

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Auth from '../layout/Auth'
 
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const arr = null
+const obj = {
+    arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+}
+// const obj = null
+
 function Home() {
     return (
         <Auth>
             <h1>Home</h1>
+            <ul>
+                {/** if(arr){arr.map((a, key) => (<li key={key}>{a}</li>))}}  */}
+                {obj?.arr && obj.arr.map((a, key) => (<li key={key}>{a}</li>))}
+            </ul>
         </Auth>
     )
 }
